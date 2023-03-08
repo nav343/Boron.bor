b6a2831bbc29dbbe00f5e2277f5ef16e05544871
nav343 <snmlogic111@gmail.com>
2023-03-08 19:56:14 +0530

    Made a small utility tool for creating a new boron project, added some new built-in variables and added support for history in repl mode which enables users to access previously typed code with up arrow

--- src/frontend/utils/initBoronProject.ts
+++ src/frontend/utils/initBoronProject.ts
@@ -1,10 +1,9 @@
-import { writeFileSync } from "fs";
+import { exec } from "child_process";
 import { exit } from "process";
-import { GREEN, RESET } from "./colors";
+import { BOLD, GREEN, RED, RESET, YELLOW } from "./colors";
 const filePrompt = require('prompt-sync')({
   history: require('prompt-sync-history')()
 });
-import { dirname } from 'path'
 export function initBoronProject() {
   const path = process.argv[3]
   if (path === 'undefined') { console.log("Expected a directory name or a ."); exit(1) }
@@ -12,24 +11,19 @@ export function initBoronProject() {
   if (typeof path == 'string') {
     if (path === '.') {
       console.log(GREEN + `Creating a boron project..... Please answer the given questions` + RESET)
-      const name = filePrompt("Name: ")
-      const description = filePrompt("Description")
-      const version = filePrompt("Version: ")
+      const name = filePrompt(YELLOW + BOLD + "Name: " + RESET)
+      const description = filePrompt(YELLOW + BOLD + "Description: " + RESET)
+      const version = filePrompt(YELLOW + BOLD + "Version: " + RESET)
       filePrompt.history.save()
-
-      // Creating directories and writing files.
-      // boronConfig file (similar to package.json)
-      const config =
-        `{
-  "name": ${name},
-  "description": ${description},
-  "version": ${version}
-}`
+      if (!name || !description || !version) { console.log(RED + BOLD + "A value is required for NAME, DESCRIPTION AND VERSION" + RESET); exit(1) }
 
       try {
-        console.log(process.cwd())
-        //writeFileSync(__dirname + '/boronConf.json', config, { encoding: 'utf8' })
-        exit(0)
+        if (process.platform === 'linux') {
+          exec(`touch ./borConfig.json`, (err) => { if (err) { console.log(err); exit(1) } })
+          exec(`echo '{\n "name": "${name}",\n "description": "${description}",\n "version": "${version}"\n}' >> ./borConfig.json`, (err) => { if (err) { console.log(err); exit(1) } })
+          exec(`mkdir src && cd ./src && echo 'print("Super cool boron project")' >> ./main.bor`, (err) => { if (err) { console.log(err); exit(1) } })
+          console.log(GREEN + BOLD + "Done !!. Happy Hacking !!" + RESET)
+        } else { console.log(RED + BOLD + `The tool is not yet supported for your operating system (${process.platform}).` + RESET); exit(0) }
       } catch (err) { console.log(err); exit(1) }
     }
   }
